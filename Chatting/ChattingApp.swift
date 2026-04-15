8bbb191463b25437d91b8b610cb4db996f4960fd
Sade El Moudden <sade.elmoudden@edu.gbsl.ch>
2026-04-15 13:16:46 +0200

    Initial Commit

---
+++ Chatting/ChattingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattingApp.swift
+//  Chatting
+//
+//  Created by Sade El Moudden on 15.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct ChattingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
